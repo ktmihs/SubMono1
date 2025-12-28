3bc2dacfb45afbef901d98ea4be0f18755dd0de5
ktmihs <yellowgreen423@gmail.com>
2025-12-28 20:53:55 +0900

    라우터 경로 구현
    - 상위로부터 props를 받아 경로를 생성할 수 있도록 구현
    - 개발 환경에서는 props가 없으므로 default 값을 /로 설정
    - props 값을 받아 동적으로 라우터 구현하도록 변경할 예정

---
+++ src/routes/createUserRoutes.jsx
@@ -0,0 +1,32 @@
+import MainLayout from '@/layouts/MainLayout';
+import HomePage from '@/pages/Home';
+import SubPage from '@/pages/Sub';
+import NotFound from '@/pages/NotFound';
+
+export function createUserRoutes(options = {}) {
+  const {
+    basePath = '/',
+    auth = true,
+  } = options;
+
+  if (!auth) return [];
+
+  return [
+    {
+      path: basePath,
+      element: <MainLayout auth={auth} />, // ✅ Layout
+      children: [{
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: `sub`,
+        element: <SubPage />,
+      }]
+    },
+    {
+      path: '*',
+      element: <NotFound />
+    }
+  ];
+}
